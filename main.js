887e1dfc80389ed6018d6384e43f13cd07a11579
anhishek1289@gmail.com <abhishek1289@gmail.com>
2022-12-13 22:10:22 +0530

    js

---
+++ main.js
@@ -0,0 +1,20 @@
+// document.getElementById('#text').innerHTML='js hanson'
+
+// document.getElementsByName('h1').innerHTML='js handson'
+
+// document.getElementsByClassName('.box').innerHTML='Js handson'
+
+// Document.getElementByTagName('#text').innerHTML='Js handson'
+
+function onbuttonclick(){
+    var flex= document.getElementsByID('#class1')
+    if(flex.style.flexDirection = "row"){
+        flex.style.flexDirection = "coloum"
+        console.log('coloum')
+    }
+    else{
+        flex.style.flexDirection = "row";
+        console.log('row');
+    }
+}
+
